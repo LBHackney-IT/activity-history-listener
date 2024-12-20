113cf9033ce8fd047cd21054cbb8afd62a2985e2
Adam <adam.tracy20@gmail.com>
2024-12-20 12:04:41 +0000

    Should not throw

--- ActivityListener.Tests/E2ETests/Steps/ProcessActivityEventSteps.cs
+++ ActivityListener.Tests/E2ETests/Steps/ProcessActivityEventSteps.cs
@@ -130,6 +130,7 @@ namespace ActivityListener.Tests.E2ETests.Steps
             var dbQuery = dbContext.QueryAsync<ActivityHistoryDB>(eventSns.EntityId);
             var resultsSet = await dbQuery.GetNextSetAsync().ConfigureAwait(false);
             resultsSet.Count.Should().Be(0);
+            _lastException.Should().BeNull();
         }
 
         private void VerifyDynamicDataObject(object expectedObj, object actualObj)
