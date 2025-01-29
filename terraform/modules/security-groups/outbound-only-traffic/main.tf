e69fb65c855c51319edac6087149e09d9ee6e1cf
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2025-01-29 11:25:17 +0000

    Add outbound-traffic security group TF module to reduce repetition.

---
+++ terraform/modules/security-groups/outbound-only-traffic/main.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "activity_listener_sg" {
+  vpc_id = var.vpc_id
+  name_prefix = "activity_listener_outgoing_traffic"
+  description = "SG used to hook activity listener lambda into VPC. No incoming traffic allowed, all outgoing traffic allowed."
+
+  egress {
+    description = "allow outbound traffic"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  # No ingress - listener does not listen to incoming traffic
+  
+  tags = {
+    Name = "activity_listener-${var.environment_name}"
+  }
+}
